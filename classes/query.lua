a0fbf8d0b2fdea0bacc6342394dcd3a5f2af5893
Xander Harrison <xander.harrison77@gmail.com>
2023-01-27 06:50:33 -0500

    Merged Query and Results classes

--- classes/query.lua
+++ classes/query.lua
@@ -27,3 +27,34 @@ function StaxDatabase.AsyncQuery(query, data)
 
   return nil
 end
+
+---@class StaxDatabaseResults
+---@field Ok boolean If the query was successful
+---@field Results table The results from the query
+StaxDatabaseResults = {}
+StaxDatabaseResults.__index = StaxDatabaseResults
+
+--- Creates a new instance of StaxDatabaseResults
+---@param queryResults table
+---@return StaxDatabaseResults | nil
+function StaxDatabaseResults.New(queryResults)
+  local databaseResults = {}
+  setmetatable(databaseResults, StaxDatabaseResults)
+
+  if not queryResults then
+    return nil
+  end
+
+  if not queryResults.ok then
+    return nil
+  end
+
+  if not queryResults.results then
+    return nil
+  end
+
+  databaseResults.Ok = queryResults.ok
+  databaseResults.Results = queryResults.results
+
+  return databaseResults
+end
